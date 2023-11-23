d6a9ff333d2de43a451b5decf20dfeac7999f703
chauqn2111 <vuchau123@gmail.com>
2023-11-23 11:22:57 +0700

    cret

---
+++ ProjectLibrary/DataAccess/NotificationDao.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectLibrary.DataAccess
+{
+    public class NotificationDao
+    {
+        private static NotificationDao instance = null;
+        private static readonly object instanceLook = new object();
+        public static NotificationDao Instance
+        {
+            get
+            {
+                lock (instanceLook)
+                {
+                    if (instance == null)
+                    {
+                        instance = new NotificationDao();
+                    }
+                    return instance;
+                }
+            }
+        }
+        public List<Notification> GetNotifications()
+        {
+            var listProducts = new List<Notification>();
+            try
+            {
+                using (var context = new CookingWebsiteContext())
+                {
+                    listProducts = context.Notifications.ToList();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return listProducts;
+        }
+        public Notification GetNotificationById(int notiId)
+        {
+            Notification n = new Notification();
+            try
+            {
+                using (var context = new CookingWebsiteContext())
+                {
+                    n = context.Notifications.SingleOrDefault(x => x.NotificationID == notiId);
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return n;
+        }
+        public void SaveNotification(Notification n)
+        {
+            try
+            {
+                using (var context = new CookingWebsiteContext())
+                {
+                    context.Notifications.Add(n);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+        public void UpdateNotification(Notification n)
+        {
+            try
+            {
+                using (var context = new CookingWebsiteContext())
+                {
+                    context.Entry<Notification>(n).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+        public void DeleteNotification(Notification n)
+        {
+            try
+            {
+                using (var context = new CookingWebsiteContext())
+                {
+                    var p1 = context.Notifications.SingleOrDefault(c => c.NotificationID == n.NotificationID);
+                    context.Notifications.Remove(p1);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
---
+++ ProjectWebAPI/Controllers/NotificationController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectLibrary.DataAccess;
+using ProjectLibrary.Repository;
+using ProjectWebAPI.Application;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProjectWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private INotificationRepository _reponse = new NotificationRepository();    
+        // GET: api/<NotificationController>
+        [HttpGet]
+        public ActionResult<IEnumerable<Notification>> GetNotifications() => _reponse.GetNotifications();
+        
+        
+
+        // GET api/<NotificationController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Notification> GetNotificationById(int id)
+        {
+            var notification = _reponse.GetNotificationById(id);
+            if (notification == null)
+            {
+                return NotFound();
+            }
+            return notification;
+        }
+
+        // POST api/<NotificationController>
+        [HttpPost]
+        public IActionResult PostNotification( NotificationDTO nDTO)
+        {
+            
+                var newNotification = new Notification
+                {
+                    NotificationID = nDTO.NotificationID,
+                    Title = nDTO.Title,
+                    Content = nDTO.Content,
+                    Date = nDTO.Date,
+                    UserId = nDTO.UserId,
+                };
+                _reponse.SaveNotification(newNotification);
+            
+                 return StatusCode(StatusCodes.Status500InternalServerError, "Error creating employee record");
+
+        }
+
+        // PUT api/<NotificationController>/5
+        [HttpPut("{id}")]
+        public IActionResult PutNotification(int id, NotificationDTO nDTO)
+        {
+            var temp = _reponse.GetNotificationById(id);
+            if (temp == null)
+            {
+                return NotFound();
+            }
+            temp.NotificationID = nDTO.NotificationID;
+            temp.Title = nDTO.Title;
+            temp.Content = nDTO.Content;
+            temp.Date = nDTO.Date;
+            temp.UserId = nDTO.UserId;
+            _reponse.UpdateNotification(temp);
+            return NoContent();
+
+            
+        }
+
+        // DELETE api/<NotificationController>/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteNotification(int id)
+        {
+            var temp = _reponse.GetNotificationById(id);
+            if (temp == null)
+            {
+                return NotFound();
+            }
+            _reponse.DeleteNotification(temp);
+            return NoContent();
+        }
+    }
+}
